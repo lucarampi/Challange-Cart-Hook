91e4fbca1a0c5b008fd6b6f1ac833bfcb577911b
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-02-15 07:42:27 -0300

    testing json server on Vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://github.com/lucarampi/Challange-Cart-Hook/server.json',
+  baseURL: 'https://cart-hook-lucarampi.vercel.app:3333',
 });
