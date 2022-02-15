34d76c56a988c4f08413dc87cd1b4dd83bc3e2b7
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-02-15 04:56:05 -0300

    test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://cart-hook-app-lucarampi.vercel.app',
+  baseURL: 'https://lucarampi.github.io/Challange-Cart-Hook/server.json',
 });
